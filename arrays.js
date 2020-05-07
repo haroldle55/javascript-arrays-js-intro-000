8142f05415124278e01c1116c0b642b48c8a09ca
Harold Lee <harry.yg.lee@gmail.com>
2020-05-07 17:34:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ var chocolateBars=['snickers','hundred grand','kikat','skittles']
 function addElementToBeginningOfArray(array,element){
   return[element,...array]
 }
-funciion destructivelyAddElementToBeginningOfArray(array,element){
+function destructivelyAddElementToBeginningOfArray(array,element){
   array.unshift('element')
   return array
 }
